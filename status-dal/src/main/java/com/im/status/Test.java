74b74fa9b63f366ac28c73c664a982ef69f64b88
zhizhuang.yang <zhizhuang.yang@lianbi.com.cn>
2017-08-10 16:08:02 +0800

    Initial commit

---
+++ status-dal/src/main/java/com/im/status/Test.java
@@ -0,0 +1,12 @@
+package com.im.status;
+
+/**
+ * Created by zhizhuang.yang on 2017/8/10.
+ */
+public class Test {
+
+    public static String getStr(){
+        return "Hello world";
+    }
+
+}
---
+++ status-service/src/main/java/status/ServiceTest.java
@@ -0,0 +1,15 @@
+package status;
+
+import com.im.status.Test;
+
+/**
+ * Created by zhizhuang.yang on 2017/8/10.
+ */
+public class ServiceTest {
+
+    public static void main(String[] args) {
+        System.out.println(Test.getStr());
+
+    }
+
+}
