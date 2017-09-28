98fe9d4a287763326530a8c11819deed27628a07
zhizhuang.yang <zhizhuang.yang@lincomb.com.cn>
2017-09-28 14:17:02 +0800

    默认页

--- status-web/src/main/java/com/im/status/controller/IndexController.java
+++ status-web/src/main/java/com/im/status/controller/IndexController.java
@@ -1,11 +1,12 @@
 package com.im.status.controller;
 
-import com.im.status.base.model.RespModel;
+import com.im.status.base.constants.Const;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by zhizhuang.yang on 2017/9/27.
@@ -14,17 +15,15 @@ import javax.servlet.http.HttpServletResponse;
 public class IndexController extends BaseController {
 
     @RequestMapping(value = "/")
-    public void index_default(HttpServletRequest request, HttpServletResponse response) {
-        RespModel respModel = new RespModel();
-        this.success(respModel);
-        writeResponse(response, respModel);
+    public void index_default(HttpServletRequest request, HttpServletResponse response)throws IOException {
+        response.setContentType(Const.JSON_CONTENT_TYPE);
+        response.getWriter().write("Hello world");
     }
 
     @RequestMapping(value = "/index")
-    public void index(HttpServletRequest request, HttpServletResponse response) {
-        RespModel respModel = new RespModel();
-        this.success(respModel);
-        writeResponse(response, respModel);
+    public void index(HttpServletRequest request, HttpServletResponse response)throws IOException {
+        response.setContentType(Const.JSON_CONTENT_TYPE);
+        response.getWriter().write("Hello world");
 
     }
 }
